03d357dd82bc764c45c27defe40bf470b63cf8bf
muna <569365548@qq.com>
2023-06-28 23:19:14 +0800

    day1

---
+++ Day01-15/code/P-Day01/hello.py
@@ -0,0 +1,13 @@
+# 开始学习python
+import turtle
+print('first day')
+turtle.pensize(4)
+turtle.pencolor('blue')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
